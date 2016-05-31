c4efedc8ac25dabce49753607f958e7993578f18
chendaole <chendaole@4399inc.com>
2016-05-31 13:33:17 +0800

    add：添加过场动画

--- app/profile/profile.jsx
+++ app/profile/profile.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-
+import { Button } from 'antd';
 // 引入各部分组件
 import Hello from './components/hello/hello.jsx';
 import Timer from './components/timer/timer.jsx';
+
+import ModalDialogTest from '../components/ModalDialogTest.jsx';
 
 import './profile.css';
 
 /*简介父组件*/
 export default class Profile extends React.Component {
+
+    handleDialog(e){
+        ModalDialogTest.show();
+    }
+
     render() {
         return (
             <div>
                 <Hello text="Web Developer"/>
-                <Timer />               
+                <Timer />
+                <Button size="large"  onClick ={ e =>  this.handleDialog(e)}> 确定</Button>
             </div>
         )
     }    
